4ebe8d6edfc2895bae2a80a6ec40065acdbdd24e
P51901205 <forncedor.CLeite@brasilprev.com.br>
2020-04-06 11:26:45 -0300

    Commit inicial....

---
+++ src/main/java/br/com/fiap/covid/helpers/HelpersApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.covid.helpers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpersApplication.class, args);
+	}
+
+}
